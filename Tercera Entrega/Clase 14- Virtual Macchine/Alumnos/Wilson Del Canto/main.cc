d89b70d675a53ca8db18732828bf78888daa5f1b
wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-11-16 22:56:31 -0300

    Entrega clase 14

---
+++ Tercera Entrega/Clase 14- Virtual Macchine/Alumnos/Wilson Del Canto/main.cc
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include <conio.h>
+main()
+{
+  int a, b, suma;
+  cout << "\n ingrese numero a:";
+  cin >> a;
+  cout << "\n ingrese numero b:";
+  cin >> b;
+  suma = a + b;
+  cout << "\n la suma de " << a << "+" << b << " es:" << suma;
+  getch();
+  return 0;
+}
